c105f9adf8a5babc1fe99f5692e85f357c4de790
bperezm <bperezm@indracompany.com>
2023-04-12 12:47:28 -0600

    Clases CSS y pagina certificación

--- santillana/urls.py
+++ santillana/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = "home"),
+    path('certificacion/', views.certificacion, name="certificacion"),
 ]
--- santillana/views.py
+++ santillana/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     template_name = 'santillana/home.html'
+    return render(request, template_name, {})
 
+
+def certificacion(request):
+    template_name = 'santillana/certificacion.html'
     return render(request, template_name, {})
