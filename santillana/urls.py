ffaa3357e3c07e69216c3eb0c7d0e7b18f656ff7
bperezm <bperezm.minsait.com>
2023-06-05 16:54:53 -0600

    Conocer

--- santillana/urls.py
+++ santillana/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('certificacion/', views.certificacion, name="certificacion"), # Terminar
-    path('estandares/', views.estandares, name="estandares"), #Refactorizar
+    path('certificacion/', views.certificacion, name="certificacion"), # Lista
+    path('estandares/', views.estandares, name="estandares"), #Lista
     path('conocer/', views.conocer, name="conocer"), #Refactorizar
     path('informacion/', views.informacion, name="informacion"), #Lista
     path('etica/', views.etica, name="etica"),#Lista
